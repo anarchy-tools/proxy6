5b6dd3d52a4e1b69f3816800e8daf0a4db16f489
anarchy-tools <anarchytools@gmail.com>
2022-10-16 11:57:41 -0400

    update

--- src.ts/cli.ts
+++ src.ts/cli.ts
@@ -1,18 +1,23 @@
 import { Proxy6Client } from "./proxy6";
 
-import yargs from 'yargs';
-import util from 'util';
+import { snakeCase } from "change-case";
+import yargs from "yargs";
+import util from "util";
 
 export async function runCLI() {
   const options = Object.assign({}, yargs.options);
-  const arg = options._[0];
-  delete options._;
-  const client = Proxy6Client.fromEnv();
-  console.log(util.inspect(await client._call(arg, Object.entries(options).reduce((r, [key, value]) => {
-    r[changecase.snakeCase(key)] = value;
-    return r;
-  }, {})), {
-    colors: true,
-    depth: 5
-  }));
+  if (options.h | options.help) {
+    console.log('usage: proxy6 <command> [options]');
+  } else {
+    const arg = options._[0];
+    delete options._;
+    const client = Proxy6Client.fromEnv();
+    console.log(util.inspect(await client._call(arg, Object.entries(options).reduce((r, [key, value]) => {
+      r[snakeCase(key)] = value;
+      return r;
+    }, {})), {
+      colors: true,
+      depth: 5
+    }));
+  }
 }
